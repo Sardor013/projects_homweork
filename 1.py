c85b1f040035806598ff062662862c72410087db
Sardor013 <sardoromonqulov8@gmail.com>
2024-04-01 23:05:13 +0500

    .

--- 1.py
+++ 1.py
@@ -2,3 +2,18 @@ print("Hello")
 
 
 c = 14
+
+n = int(input())
+
+student = {}
+
+for i in range(n):
+    name, *marks = input().split()
+    marks = list(map(float, marks))
+    student[name] = marks
+
+query_name = input()
+
+average_marks = sum(student[query_name]) / len(student[query_name])
+
+print("{:.2f}".format(average_marks))
